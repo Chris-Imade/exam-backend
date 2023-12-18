d2efc209f2f1324cb292ab23093693e14e5133df
@Chrisdevcode <jephthahimade@gmail.com>
2023-12-18 20:14:10 +0100

    finalized

--- server.js
+++ server.js
@@ -11,12 +11,12 @@ const PORT = process.env.PORT || 6790;
 
 // Middleware for parsing JSON
 app.use(bodyParser.json());
-app.use(cors({ origin: "http://localhost:3000/" }));
+app.use(cors({ origin: "http://localhost:3000" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ limit: '10mb' }));
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000/");
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
     next();
